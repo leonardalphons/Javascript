ec5923bb6fd5a711cc7f5c77c8b475e84ca3202c
Leonard <leonardalphons@gmail.com>
2025-12-20 23:51:14 +0530

    Add Javascript Practice Files

--- first.js
+++ first.js
@@ -1,2 +1,21 @@
-let heading = document.getElementById("heading-id");
-console.dir(heading);
+let button = document.querySelector("#btn");
+const body = document.querySelector("body");
+
+let mode = "light";
+
+const lightMode = () => {
+  console.log("Button was clicked...");
+  if (mode === "light") {
+    console.log(" mode is dark ");
+    mode = "dark";
+    body.classList.add("dark");
+    body.classList.remove("light");
+  } else {
+    console.log(" mode is light");
+    mode = "light";
+    body.classList.add("light");
+    body.classList.remove("dark");
+  }
+};
+
+button.addEventListener("click", lightMode);
